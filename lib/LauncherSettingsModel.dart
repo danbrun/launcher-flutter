86cf40d8fec1038cc9977b081bb540410cae7a21
Dan Brunwasser <dan@brunwasser.com>
2021-03-30 00:18:01 -0700

    Added settings persistence.

--- lib/LauncherSettingsModel.dart
+++ lib/LauncherSettingsModel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LauncherSettingsModel extends ChangeNotifier {
   double _iconSize = 32;
@@ -9,18 +10,35 @@ class LauncherSettingsModel extends ChangeNotifier {
   double get itemPadding => _itemPadding;
   int get listColumns => _listColumns;
 
+  SharedPreferences? _preferences;
+
+  LauncherSettingsModel() {
+    SharedPreferences
+      .getInstance()
+      .then((preferences) {
+        _preferences = preferences;
+        _iconSize = preferences.getDouble('iconSize') ?? _iconSize;
+        _itemPadding = preferences.getDouble('itemPadding') ?? _itemPadding;
+        _listColumns = preferences.getInt('listColumns') ?? _listColumns;
+        notifyListeners();
+      });
+  }
+
   void setIconSize(double iconSize) {
     _iconSize = iconSize;
+    _preferences?.setDouble('iconSize', _iconSize);
     notifyListeners();
   }
 
   void setItemPadding(double itemPadding) {
     _itemPadding = itemPadding;
+    _preferences?.setDouble('itemPadding', _itemPadding);
     notifyListeners();
   }
 
   void setListColumns(int listColumns) {
     _listColumns = listColumns;
+    _preferences?.setInt('listColumns', _listColumns);
     notifyListeners();
   }
 }
