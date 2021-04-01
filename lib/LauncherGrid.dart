e63e5632f838c90bb27917bbce3047e556a2021c
Dan Brunwasser <dan@brunwasser.com>
2021-03-31 23:28:07 -0700

    Refactored app grid.

--- lib/LauncherGrid.dart
+++ lib/LauncherGrid.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:launcher/DeviceAppsModel.dart';
-import 'package:launcher/FlexHeightGrid.dart';
 import 'package:launcher/LauncherSettingsModel.dart';
 import 'package:provider/provider.dart';
 
@@ -11,15 +10,16 @@ class LauncherGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer2<DeviceAppsModel, LauncherSettingsModel>(
       builder: (context, deviceAppsModel, launcherSettingsModel, child) {
-        return FlexHeightGrid(
-          itemBuilder: (builder, index) => LauncherItem(
-            deviceAppsModel.apps[index],
-            launcherSettingsModel.iconSize,
-            launcherSettingsModel.itemPadding,
-          ),
+        double itemWidth = MediaQuery.of(context).size.width / launcherSettingsModel.listColumns;
+        double itemHeight = launcherSettingsModel.iconSize + 2 * launcherSettingsModel.itemPadding;
+        return GridView.builder(
+          itemBuilder: (builder, index) => LauncherItem(deviceAppsModel.apps[index]),
           itemCount: deviceAppsModel.apps.length,
-          columns: launcherSettingsModel.listColumns,
           padding: MediaQuery.of(context).viewPadding + EdgeInsets.only(bottom: 16),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: launcherSettingsModel.listColumns,
+            childAspectRatio: itemWidth / itemHeight,
+          ),
         );
       },
     );
@@ -28,40 +28,42 @@ class LauncherGrid extends StatelessWidget {
 
 class LauncherItem extends StatelessWidget {
   final LauncherApplication _app;
-  final double _iconSize;
-  final double _itemPadding;
 
-  LauncherItem(this._app, this._iconSize, this._itemPadding);
+  LauncherItem(this._app);
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => _app.openApp(),
-      onDoubleTap: () => _app.openSettings(),
-      child: Padding(
-        child: Row(
-          children: [
-            Padding(
-              child: Image(
-                image: _app.icon,
-                width: _iconSize,
-                height: _iconSize,
-              ),
-              padding: EdgeInsets.only(right: _itemPadding),
-            ),
-            Flexible(
-              child: Text(
-                _app.name,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  color: Colors.white,
+    return Consumer<LauncherSettingsModel>(
+      builder: (context, launcherSettingsModel, child) {
+        return InkWell(
+          onTap: () => _app.openApp(),
+          onDoubleTap: () => _app.openSettings(),
+          child: Padding(
+            child: Row(
+              children: [
+                Padding(
+                  child: Image(
+                    image: _app.icon,
+                    width: launcherSettingsModel.iconSize,
+                    height: launcherSettingsModel.iconSize,
+                  ),
+                  padding: EdgeInsets.only(right: launcherSettingsModel.itemPadding),
                 ),
-              ),
+                Flexible(
+                  child: Text(
+                    _app.name,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-        padding: EdgeInsets.all(_itemPadding),
-      ),
+            padding: EdgeInsets.all(launcherSettingsModel.itemPadding),
+          ),
+        );
+      }
     );
   }
 }
